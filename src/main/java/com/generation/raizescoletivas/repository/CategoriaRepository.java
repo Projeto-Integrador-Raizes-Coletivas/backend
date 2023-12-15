aef052555a6ec49e6dab56935b5efb5a8334d44e
amandats4i <amandatsai16@gmail.com>
2023-12-15 10:04:17 -0300

    Criar Categoria Repository

---
+++ src/main/java/com/generation/raizescoletivas/repository/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.generation.raizescoletivas.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.generation.raizescoletivas.model.Categoria;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+	
+	
+
+}
