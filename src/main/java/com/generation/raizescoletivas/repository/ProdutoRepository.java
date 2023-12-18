bc810d8b015600d636117d17be370427dd690525
Gabriel Rodrigues <gabriel.rodriguesxs@gmail.com>
2023-12-18 09:56:19 -0300

    Adiciona a Interface de Produto

---
+++ src/main/java/com/generation/raizescoletivas/repository/ProdutoRepository.java
@@ -0,0 +1,7 @@
+package com.generation.raizescoletivas.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+
+}
